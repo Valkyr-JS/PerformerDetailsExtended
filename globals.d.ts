58d77091a08c14972d48cd3d3799826e77a9e235
ValkyrJS <valkyr.photos@gmail.com>
2024-05-26 15:30:13 +0100

    chore: StashGQLPerformer type

--- globals.d.ts
+++ globals.d.ts
@@ -1,11 +1,19 @@
-interface Window {
+declare interface Window {
   PluginApi: IPluginApi;
 }
 
 interface IPluginApi {
   React: typeof React;
-  ReactDOM: typeof ReactDOM,
-  GQL: any;
+  ReactDOM: typeof ReactDOM;
+  GQL: {
+    useFindPerformerQuery({ variables: { id: number } }): {
+      data: {
+        findPerformer: {
+          scenes: StashGQLScenes[];
+        };
+      };
+    };
+  };
   Event: {
     addEventListener: (
       event: string,
@@ -46,4 +54,78 @@ interface IPluginApi {
   register: {
     route: (path: string, component: React.FC<any>) => void;
   };
+}
+
+/* -------------------------------------------------------------------------- */
+/*                                  Stash GQL                                 */
+/* -------------------------------------------------------------------------- */
+
+interface StashGQLMovie {}
+interface StashGQLPerformer {
+  alias_list: string[string];
+  created_at: Date;
+  favorite: boolean;
+  gallery_count: number;
+  id: string;
+  ignore_auto_tag: boolean;
+  image_count: number;
+  movie_count: number;
+  movies: StashGQLMovie[];
+  performer_count: number;
+  name: string;
+  scene_count: number;
+  scenes: StashGQLScenes[];
+  stash_ids: StashGQLStashID[];
+  tags: StashGQLTags[];
+  updated_at: Date;
+
+  birthdate?: string;
+  career_length?: string;
+  country?: string;
+  disambiguation?: string;
+  ethnicity?: string;
+  eye_color?: string;
+  circumcised?: StashGQLCircumisedEnum;
+  death_date?: string;
+  details?: string;
+  fake_tits?: string;
+  gender?: StashGQLGender;
+  hair_color?: string;
+  height_cm?: number;
+  image_path?: string;
+  instagram?: string;
+  measurements?: string;
+  o_counter?: number;
+  piercings?: string;
+  penis_length?: number;
+  rating100?: number;
+  tattoos?: string;
+  twitter?: string;
+  url?: string;
+  weight?: number;
+}
+
+interface StashGQLScenes {
+  performers: StashGQLPerformer[];
+}
+
+interface StashGQLStashID {
+  endpoint: string;
+  stash_id: string;
+}
+
+interface StashGQLTags {}
+
+enum StashGQLCircumisedEnum {
+  CUT = 0,
+  UNCUT,
+}
+
+enum StashGQLGenderEnum {
+  MALE = 0,
+  FEMALE,
+  TRANSGENDER_MALE,
+  TRANSGENDER_FEMALE,
+  INTERSEX,
+  NON_BINARY,
 }
