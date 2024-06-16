b6b4bf3991373c523beadb9ee6584666c3f8b31a
ValkyrJS <valkyr.photos@gmail.com>
2024-06-16 13:27:35 +0100

    docs: Ref TagCard

--- src/components/TagItem.tsx
+++ src/components/TagItem.tsx
@@ -3,6 +3,10 @@ const { React } = window.PluginApi;
 /** A custom tag item component designed to look like the native component, but
  * with more customisation options. */
 const TagItem: React.FC<TagItemProps> = (props) => {
+  /**
+   * ! Awaiting `TagCard` component to be made available in order to add the
+   * hover popover.
+   */
   return (
     <span className="tag-item badge badge-secondary">
       <a href={props.link}>{props.title}</a>
