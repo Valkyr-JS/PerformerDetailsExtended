a42db3f5f9d56da94b989cefd34c1e5bd2574834
ValkyrJS <valkyr.photos@gmail.com>
2024-07-06 12:25:01 +0100

    Optional TotalPlayCount item

--- src/components/ItemTotalPlayDuration.tsx
+++ src/components/ItemTotalPlayDuration.tsx
@@ -9,7 +9,7 @@ const ItemTotalPlayDuration: React.FC<ItemTotalPlayDurationProps> = ({
   ...props
 }) => {
   const { totalPlayCountOn } = props.pluginConfig;
-  const { total_play_duration } = props.statsQueryResult;
+  const { total_play_count, total_play_duration } = props.statsQueryResult;
   let playCount = 0;
   let playDuration = 0;
 
@@ -25,8 +25,8 @@ const ItemTotalPlayDuration: React.FC<ItemTotalPlayDurationProps> = ({
     Math.round((playDuration / total_play_duration + Number.EPSILON) * 10000) /
     100;
   const additionalDurationData = {
-    id: "duration-percentage",
-    value: `${percentage}% of ${createDuration(total_play_duration)}`,
+    id: "play-duration-percentage",
+    value: `${percentage}% of ${createDuration(total_play_duration)} total`,
   };
 
   // Only show the additional duration data if it is more than 0
@@ -44,15 +44,23 @@ const ItemTotalPlayDuration: React.FC<ItemTotalPlayDurationProps> = ({
     ? additionalDurationData
     : additionalCountData;
   return (
-    <DetailItem
-      collapsed={collapsed}
-      dataValue={playDuration}
-      id="total-play-duration"
-      title="Total Play Duration"
-      value={createDuration(playDuration)}
-      wide={true}
-      additionalData={additionalData}
-    />
+    <>
+      <DetailItem
+        collapsed={collapsed}
+        dataValue={playDuration}
+        id="total-play-duration"
+        title="Total Play Duration"
+        value={createDuration(playDuration)}
+        wide={true}
+        additionalData={additionalData}
+      />
+      <ItemTotalPlayCount
+        collapsed={collapsed}
+        playCount={playCount}
+        show={totalPlayCountOn}
+        totalPlayCount={total_play_count}
+      />
+    </>
   );
 };
 
@@ -67,4 +75,46 @@ interface ItemTotalPlayDurationProps {
   scenesQueryResult: FindScenesResultType;
   /** The `stats` data object returned from the GQL query. */
   statsQueryResult: StatsResultType;
+}
+
+/* -------------------------------------------------------------------------- */
+/*                            Total Play Count item                           */
+/* -------------------------------------------------------------------------- */
+
+const ItemTotalPlayCount: React.FC<ItemTotalPlayCountProps> = ({
+  playCount,
+  totalPlayCount,
+  ...props
+}) => {
+  if (!props.show) return null;
+
+  const percentage =
+    Math.round((playCount / totalPlayCount + Number.EPSILON) * 10000) / 100;
+  const additionalData = {
+    id: "play-count-percentage",
+    value: `${percentage}% of ${totalPlayCount} total`,
+  };
+
+  return (
+    <DetailItem
+      collapsed={props.collapsed}
+      dataValue={playCount}
+      id="total-play-count"
+      title="Total Play Count"
+      value={playCount}
+      wide={true}
+      additionalData={additionalData}
+    />
+  );
+};
+
+interface ItemTotalPlayCountProps {
+  /** Identifies whether the PerformerDetailsPanel is currently collapsed. */
+  collapsed: PropsPerformerDetailsPanelDetailGroup["collapsed"];
+  /** The total number of plays across the performer's scenes. */
+  playCount: number;
+  /** Identifies whether the item should be rendered or not. */
+  show: boolean;
+  /** The total number of plays across the user's library. */
+  totalPlayCount: number;
 }
