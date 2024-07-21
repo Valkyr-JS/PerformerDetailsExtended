e8dc5c49cfaf6b8b10b3fbc839de850cf71c1baf
ValkyrJS <valkyr.photos@gmail.com>
2024-07-21 22:58:38 +0100

    Webpack and Typescript updates

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,42 +1,7 @@
-const CopyPlugin = require("copy-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const path = require("path");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
 
-module.exports = {
-  entry: "./src/main.tsx",
-  output: {
-    filename: "PerformerDetailsExtended.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
+module.exports = merge(common, {
+  devtool: "eval",
   mode: "development",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.scss$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-      },
-    ],
-  },
-  plugins: [
-    new CopyPlugin({
-      patterns: [
-        {
-          from: "src/source.yml",
-          to: "PerformerDetailsExtended.yml",
-        },
-      ],
-    }),
-    new MiniCssExtractPlugin({
-      filename: "PerformerDetailsExtended.css",
-    }),
-  ],
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-};
+});
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,48 +1,12 @@
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const path = require("path");
+const { merge } = require("webpack-merge");
 const TerserPlugin = require("terser-webpack-plugin");
+const common = require("./webpack.common.js");
 
-module.exports = {
-  entry: "./src/main.tsx",
-  output: {
-    filename: "PerformerDetailsExtended.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
+module.exports = merge(common, {
   mode: "production",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.scss$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-      },
-    ],
-  },
   optimization: {
     minimize: true,
     minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
   },
-  plugins: [
-    new CopyPlugin({
-      patterns: [
-        {
-          from: "src/source.yml",
-          to: "PerformerDetailsExtended.yml",
-        },
-      ],
-    }),
-    new MiniCssExtractPlugin({
-      filename: "PerformerDetailsExtended.css",
-    }),
-  ],
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-};
+});
